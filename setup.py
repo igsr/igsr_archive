cbb29f7ab102fca98575f4abe03fd8cb3b6e34ca
elowy01 <ernestolowy@gmail.com>
2020-06-24 16:23:41 +0100

    Bump 1 version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = 'https://github.com/igsr/igsr_archive/archive/0.1.8.tar.gz'
 EMAIL = 'ernestolowy@gmail.com'
 AUTHOR = 'Ernesto Lowy'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.8'
+VERSION = '0.1.9'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
