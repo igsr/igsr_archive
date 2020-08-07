2fbe9cb97c622f0b88edadcb91e27650b0a31914
elowy01 <ernestolowy@gmail.com>
2020-08-07 17:34:00 +0100

    deleting files

--- tests/env.sh
+++
@@ -1,5 +0,0 @@
-export PYTHONPATH=/Users/ernesto/PycharmProjects/igsr_archive/
-export USERNAME=g1k-ernesto
-export FIREPWD=52LWlySa6G96nnqdbciU
-export DBNAME=g1k_archive_staging_track
-export DBPWD=thousandgenomes
