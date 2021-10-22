7cca14a2ed58845e2101968098859dddeab627fc
Ernesto Lowy <ernestolowy@gmail.com>
2021-10-22 14:17:57 +0100

    New release

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ from setuptools import find_packages, setup, Command
 NAME = 'igsr_archive'
 DESCRIPTION = 'Contains code to perform several operations involving files ' \
               'in the IGSR (1000 genomes) FTP archive.'
-URL = 'https://github.com/igsr/igsr_archive/archive/refs/tags/0.2.9.tar.gz'
+URL = 'https://github.com/igsr/igsr_archive/archive/refs/tags/0.3.0.tar.gz'
 EMAIL = 'ernestolowy@gmail.com'
 AUTHOR = 'Ernesto Lowy'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.9'
+VERSION = '0.3.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
